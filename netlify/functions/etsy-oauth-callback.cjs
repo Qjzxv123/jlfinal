f08755c0b4c2be34d11abfdccb9d1b6d297f79e2
Aidan Mcclain <qjzxv123@gmail.com>
2025-07-29 14:10:15 -0400

    Update favicon link in Calendar.html to use absolute path for consistency

--- netlify/functions/etsy-oauth-callback.cjs
+++ netlify/functions/etsy-oauth-callback.cjs
@@ -71,7 +71,7 @@ exports.handler = async (event) => {
       refresh_token: tokenData.refresh_token,
       expires_at: Math.floor(Date.now() / 1000) + (tokenData.expires_in || 0),
       user_key: userInfo.user_display_name || null
-    }, { onConflict: ['UserID', 'platform'] });
+    });
     console.log('[ETSY OAUTH CALLBACK] Upsert result:', { data, error });
   }
 
